8d537c74ac149a20e16eee1aacaac92bacaf95fe
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2022-11-08 16:09:56 -0500

    3.3 changes

--- Assets/Course Library/scripts/PlayerController.cs
+++ Assets/Course Library/scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {   
     //privates
     private Rigidbody playerRb;
+    private Animator playerAnim;
     
     //publics
     public float jumpForce;
@@ -15,21 +16,23 @@ public class PlayerController : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        playerRb = GetComponent<Rigidbody>();
-        
-        //The *= means original value muliplied and is equal to gravity modifier.
+    {   //The *= means original value muliplied and is equal to gravity modifier.
         Physics.gravity *= gravityModifier;
+
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //When Space is pressed, If the player is on the ground, player jumps.
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -45,7 +48,9 @@ public class PlayerController : MonoBehaviour
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
-            Debug.Log("HAHAHAHA BYE BYE!");
+
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
